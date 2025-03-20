554b02557df82393bc894a45a399c7aa164d6610
seonjuuu <mhnaa6469@gmail.com>
2025-03-20 23:09:24 +0900

    [add] TextAnalysisService

---
+++ 2_SpringBoot/src/main/java/com/SpringBoot/__SpringBoot/service/TextAnalysisService.java
@@ -0,0 +1,16 @@
+package com.SpringBoot.__SpringBoot.service;
+
+import com.SpringBoot.__SpringBoot.dto.TextResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TextAnalysisService {
+
+    public TextResponseDto analyzeText(String sentence) {
+        int length = sentence.length();
+        int wordCount = sentence.split("\\s+").length;  // " \\s+ " : 공백(스페이스, 탭, 줄 바꿈 등)을 연속된 개수에 상관없이 하나의 구분자로 사용
+        boolean containSpring = sentence.toLowerCase().contains("spring");
+
+        return new TextResponseDto(length, wordCount, containSpring);
+    }
+}
