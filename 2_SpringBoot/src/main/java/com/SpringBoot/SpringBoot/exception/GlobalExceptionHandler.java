5a3ea79b731b4029c760ccd75d2dad1e07e8db4e
seonjuuu <mhnaa6469@gmail.com>
2025-03-21 01:48:45 +0900

    [add] GlobalExceptionHandler

---
+++ 2_SpringBoot/src/main/java/com/SpringBoot/SpringBoot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.SpringBoot.SpringBoot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice  // 🔴 예외를 전역적으로 처리하는 클래스
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)  // 🔴 유효성 검사 예외 처리
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
