ae5f0bbc695e61bacf66e58cd5540382f00edcb2
seonjuuu <mhnaa6469@gmail.com>
2025-03-21 00:53:53 +0900

    [add] TextAnalysisController

--- 2_SpringBoot/src/main/java/com/SpringBoot/SpringBoot/controller/TextAnalysisController.java
+++ 2_SpringBoot/src/main/java/com/SpringBoot/SpringBoot/controller/TextAnalysisController.java
@@ -12,15 +12,16 @@ public class TextAnalysisController {
 
     // 의존성 주입
     // Spring이 TextAnalysisService 객체를 자동으로 생성해주고 연결해 줌(Spring의 @Service 덕분에 자동으로 주입됨!)
-    //
-    private final TextAnalysisService textAnalysisService;
+    // private final TextAnalysisService textAnalysisService = new TextAnalysisService(); // 직접 생성
+    private final TextAnalysisService textAnalysisService;  // 1. 필드 선언
 
+    // 2. 생성자를 통한 의존성 주입
     public TextAnalysisController(TextAnalysisService textAnalysisService) {
         this.textAnalysisService = textAnalysisService;
     }
 
     @PostMapping("/analyze")
-    public TextResponseDto analyzeText(@Valid @RequestBody TextRequestDto textRequestDto) {
+    public TextResponseDto analyzeText(@Valid @RequestBody TextRequestDto textRequestDto) {  // @RequestBody : JSON -> TextRequestDto , @Valid - @NotBlank
         return textAnalysisService.analyzeText(textRequestDto.getSentence());
     }
 }
