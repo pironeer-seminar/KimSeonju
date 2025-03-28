5672556e378cfe13ea403329f8d358e899088722
seonjuuu <mhnaa6469@gmail.com>
2025-03-28 15:34:58 +0900

    [feat] OrderItem

--- 3_SpringJpa/src/main/java/com/example/pironeer/domain/OrderItem.java
+++ 3_SpringJpa/src/main/java/com/example/pironeer/domain/OrderItem.java
@@ -1,6 +1,43 @@
 package com.example.pironeer.domain;
 
-public record OrderItem(
-        long productId,
-        int amount
-){}
+import jakarta.persistence.*;
+import org.aspectj.weaver.ast.Or;
+
+@Entity
+public class OrderItem {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        public Long id;
+
+        @ManyToOne
+        @JoinColumn(name = "order_id")
+        public Order order;
+
+        @ManyToOne
+        @JoinColumn(name = "product_id")
+        public Product product;
+
+        public int quantity;    // 주문 수량
+        public int orderPrice; // 상품 가격
+
+        protected OrderItem(){};
+
+        public OrderItem(Product product, int quantity, int order_price){  // 어떤 상품을 주문했지 알아야하므로, Product 받아와야함
+                this.product = product;
+                this.quantity = quantity;
+                this.orderPrice = order_price;
+        }
+
+        // 재고 복구
+        public void cancel() {
+                product.addAmount(quantity);
+        }
+
+        // 총 가격
+        public int getTotalPrice() {
+                return quantity * orderPrice;
+        }
+
+
+
+}
