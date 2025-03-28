445167cc671b6e63339adb48b0cfaa19289da9a0
seonjuuu <mhnaa6469@gmail.com>
2025-03-28 14:21:37 +0900

    [feat] Order

--- 3_SpringJpa/src/main/java/com/example/pironeer/domain/Order.java
+++ 3_SpringJpa/src/main/java/com/example/pironeer/domain/Order.java
@@ -2,6 +2,10 @@ package com.example.pironeer.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -9,21 +13,38 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
 
+    // User와의 연관관계
     @ManyToOne
-    public Product product;
-
-    @ManyToOne
+    @JoinColumn(name = "user_id")
     public User user;
-    public String status;
-    public int amount;
+
+    //OrderItem과의 연관관계
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    public List<OrderItem> orderItemList = new ArrayList<>();  // 현재 주문(Order)에 포함된 모든 상품(OrderItem) 을 담는
+
+    public String status;  // 주문상태 (ORDERED, CANCELED)
+    public LocalDateTime orderDate;  // 주문날짜
 
     protected Order(){}
 
     public Order(User user, Product product, String status, int amount){
         this.user = user;
-        this.product = product;
         this.status = status;
-        this.amount = amount;
+        this.orderDate = LocalDateTime.now();
+    }
+
+    // 주문상품 하나를 주문에 등록
+    public void addOrderItem(OrderItem orderItem){
+        orderItemList.add(orderItem); // 주문상품 추가
+        orderItem.order = this;  //OrderItem 입장에서는 자신의 order 필드가 어떤 주문에 속해 있는지 나타냄, 지금 이 주문(this)을 OrderItem에게 “너는 나한테 속한 거야” 라고 알려주는 것
+    }
+
+    // 주문 취소
+    public void cancel(){
+        this.status = "CANCELED";
+        for (OrderItem item : orderItemList) {
+            item.cancel();
+        }
     }
 
     public String getStatus() {
