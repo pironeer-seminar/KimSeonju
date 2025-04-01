1998faa83fb845d7479c55f60873d1cea328a408
seonjuuu <mhnaa6469@gmail.com>
2025-04-02 00:13:04 +0900

    [feat] CommentRes

--- pironeer4/src/main/java/com/example/pironeer4/dto/response/CommentRes.java
+++ pironeer4/src/main/java/com/example/pironeer4/dto/response/CommentRes.java
@@ -1,4 +1,16 @@
 package com.example.pironeer4.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class CommentRes {
+    private Long id;
+    private Long userId;
+    private Long postId;
+    private String content;
+    private LocalDateTime createdAt;
 }
