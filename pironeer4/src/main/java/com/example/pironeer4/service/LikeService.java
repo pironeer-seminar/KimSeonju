22c15dbc8115a86fc81c2177dd8132e4c8908f15
seonjuuu <mhnaa6469@gmail.com>
2025-04-02 22:54:09 +0900

    [feat] Transactional 수정

--- pironeer4/src/main/java/com/example/pironeer4/service/LikeService.java
+++ pironeer4/src/main/java/com/example/pironeer4/service/LikeService.java
@@ -4,18 +4,19 @@ import com.example.pironeer4.entity.Like;
 import com.example.pironeer4.entity.Post;
 import com.example.pironeer4.entity.User;
 import com.example.pironeer4.repository.LikeRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+
 public class LikeService {
 
     private final LikeRepository likeRepository;
 
     // 좋아요 생성
+    @Transactional
     public void likePost(User user, Post post) {
         if (likeRepository.existByUserAndPost(user, post)) {
             throw new IllegalArgumentException("이미 좋아요 존재");
@@ -25,6 +26,7 @@ public class LikeService {
     }
 
     // 좋아요 해제
+    @Transactional
     public void unlikePost(User user, Post post) {
         if (!likeRepository.existByUserAndPost(user, post)) {
             throw new IllegalArgumentException("좋아요 누른 적 없음");
@@ -33,6 +35,7 @@ public class LikeService {
     }
 
     // 좋아요 수
+    @Transactional(readOnly = true)
     public int countLikes(Post post) {
         return likeRepository.countByPost(post);
     }
