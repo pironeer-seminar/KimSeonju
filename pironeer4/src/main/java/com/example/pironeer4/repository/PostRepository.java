4dd954ccf3af766dbb34335372083a283eb96ac9
seonjuuu <mhnaa6469@gmail.com>
2025-04-03 00:02:08 +0900

    [feat] 특정 유저가 작성한 모든 게시글 조회

--- pironeer4/src/main/java/com/example/pironeer4/repository/PostRepository.java
+++ pironeer4/src/main/java/com/example/pironeer4/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.pironeer4.repository;
 
 import com.example.pironeer4.entity.Post;
 import com.example.pironeer4.entity.PostStatus;
+import com.example.pironeer4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByStatus(PostStatus status);  // SELECT * FROM post WHERE status = ?;
+
+    List<Post> findAllByUser(User user); // 특정 유저가 작성한 모든 게시글 조회
 }
