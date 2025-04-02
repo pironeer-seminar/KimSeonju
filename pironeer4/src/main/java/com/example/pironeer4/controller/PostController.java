c662cc48a900874c2883a155c63cca4894ea472c
seonjuuu <mhnaa6469@gmail.com>
2025-04-03 00:06:15 +0900

    [feat] 특정 유저가 작성한 모든 게시글 조회

--- pironeer4/src/main/java/com/example/pironeer4/controller/PostController.java
+++ pironeer4/src/main/java/com/example/pironeer4/controller/PostController.java
@@ -4,7 +4,10 @@ import com.example.pironeer4.dto.request.PostCreateReq;
 import com.example.pironeer4.dto.request.PostUpdateReq;
 import com.example.pironeer4.dto.response.PostDetailRes;
 import com.example.pironeer4.dto.response.PostSearchRes;
+import com.example.pironeer4.entity.Post;
+import com.example.pironeer4.entity.User;
 import com.example.pironeer4.service.PostService;
+import com.example.pironeer4.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final UserService userService;
 
     // 생성
     @PostMapping("")
@@ -51,5 +55,12 @@ public class PostController {
             @PathVariable("postId") Long postId
     ) {
         return postService.delete(postId);
+    }
+
+    //특정 유저가 작성한 모든 게시글 조회
+    @GetMapping("/users/{userId}/posts")
+    public List<Post> getPostsByUser(@PathVariable Long userId) {
+        User user = userService.findById(userId);
+        return postService.findAllByUser(user);
     }
 }
